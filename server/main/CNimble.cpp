32cefb8e8feda1f93123142f8c1025ae3fda2e8d
prevLanky <tmp>
2024-02-04 20:26:27 +0100

    can assign server address

--- server/main/CNimble.cpp
+++ server/main/CNimble.cpp
@@ -7,15 +7,48 @@ namespace nimble
 
     namespace 
     {
-        static void server_on_reset_cb(int reason) 
+
+        #define SERVER_TAG "BLE_SERVER" // used for ESP_LOG
+
+
+        // static variables
+        static uint8_t serverAddrType;
+
+        // Callback function
+        static void nimble_on_reset_handle(int reason) 
         {
             LOG_FATAL_FMT("ESP ERROR. Reset due to %d\n", reason); // something fucked up happend
         }
 
-       static void nimble_on_sync_cb(void) 
+       static void nimble_on_sync_handle(void) 
        {
+            // Why is this code never triggered
 
-       }
+            int returnCode;
+            returnCode = ble_hs_util_ensure_addr(RND_ADDR); // TODO: 1 doenst work for some reason 
+            assert(returnCode == 0);
+//
+            returnCode = ble_hs_id_infer_auto(0, &serverAddrType); // random address = private address? what does 0 mean? what is 1?
+            std::printf("asd1\n %d\n", serverAddrType); // its a 1
+            if (returnCode != 0) {
+                LOG_FATAL_FMT("No address was able to be created for the server %d\n", returnCode); // todo how to log this properly?
+                return;
+            }
+        //
+            uint8_t addr_val[6] {0u};
+            returnCode = ble_hs_id_copy_addr(serverAddrType, addr_val, NULL);
+            if (returnCode != 0) 
+            {
+                LOG_WARN_FMT("Adress was unable to be retrieved %d\n", returnCode);
+            }
+                ESP_LOGE(SERVER_TAG, "Address is: \n");
+
+            // u8p = addr;MODLOG_DFLT(INFO, "%02x:%02x:%02x:%02x:%02x:%02x", u8p[5], u8p[4], u8p[3], u8p[2], u8p[1], u8p[0]);
+
+    
+            //bleprph_advertise();
+            
+        }
 
 
     } // namespace
@@ -23,18 +56,21 @@ namespace nimble
 
     CNimble::CNimble() 
     {
+       
+
         std::printf("asdasd\nasdasdasd\nasdasdasd\n");
         esp_err_t errorCode = nimble_port_init(); //  Initialize controller and NimBLE host stack
         if (errorCode != ESP_OK) {
             return;
         }
 
+        ble_hs_cfg.reset_cb = nimble_on_reset_handle; // needs to be static why Christoffer teach me
+        ble_hs_cfg.sync_cb = nimble_on_sync_handle; // callback when host and controller become synced
         // - Initilize host configurations before initilizing gap and gatt
         // - security manager is also configured before initilizing gap and gatt
         // - Order is important
 
-        ble_hs_cfg.reset_cb = nimble_on_reset_cb; // needs to be static why Christoffer teach me HOW TO TEST THIS
-        ble_hs_cfg.sync_cb = nimble_on_sync_cb; // callback when host and controller become synced
+        nimble_port_run(); // the callbacks will be called in here
 
 
 
--- server/main/CNimble.h
+++ server/main/CNimble.h
@@ -5,9 +5,14 @@
 /* Project */
 #include "defines.hpp"
 
+#include "esp_log.h"
+
+
 /* BLE */
 #include "nimble/nimble_port.h"
 #include "host/ble_hs.h"
+#include "esp_netif_ip_addr.h"
+#include "host/util/util.h"
 
 //#include "nimble/nimble_port_freertos.h"
 //#include "host/ble_hs.h"
@@ -20,10 +25,19 @@
 namespace nimble 
 {
 
+    // is it possible to define consts?
+    // should we even have defines?
+    // should these be in header or cpp?
+    // Server address defines
+    #define RND_ADDR 1
+    #define PUB_ADDR 0
+
+
+
     namespace 
     {
-        static void nimble_on_reset_cb(int reason);
-        static void nimble_on_sync_cb(void);
+        static void nimble_on_reset_handle(int reason);
+        static void nimble_on_sync_handle(void);
 
     } // namespace
 
