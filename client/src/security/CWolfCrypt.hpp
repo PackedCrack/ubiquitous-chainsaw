0bbbac35a5b222d70d4742b768c2a50b82de0d52
christoffer larsson <christofferlarsson38@hotmail.com>
2024-02-17 23:01:11 +0100

    Added simple RAII wrapper for the wolfcrypt library's init and cleanup functions

---
+++ client/src/security/CWolfCrypt.hpp
@@ -0,0 +1,27 @@
+//
+// Created by qwerty on 2024-02-17.
+//
+
+#pragma once
+
+
+namespace security
+{
+class CWolfCrypt
+{
+public:
+    enum class Error
+    {
+        constructorFailure
+    };
+public:
+    [[nodiscard]] static std::expected<CWolfCrypt*, Error> instance();
+    ~CWolfCrypt();
+    CWolfCrypt(const CWolfCrypt& other) = delete;
+    CWolfCrypt(CWolfCrypt&& other) = delete;
+    CWolfCrypt& operator=(const CWolfCrypt& other) = delete;
+    CWolfCrypt& operator=(CWolfCrypt&& other) = delete;
+private:
+    CWolfCrypt();
+};
+}   // namespace security
