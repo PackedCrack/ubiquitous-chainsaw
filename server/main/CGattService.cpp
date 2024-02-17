ec32d26ef3ffbf0ad0b8a1846cce28120994834d
prevLanky <tmp>
2024-02-17 20:31:47 +0100

    extracted gatt service registring to a func, addded p to ptr variables

--- server/main/CGattService.cpp
+++ server/main/CGattService.cpp
@@ -8,60 +8,56 @@ namespace
 {
 
 
-[[Nodiscard]] ble_gatt_chr_def make_characteristic(const ble_uuid_t* uuid, ble_gatt_access_fn callbackFunc,
-                                       ble_gatt_dsc_def* descriptors, uint8_t flags, uint16_t* handle) 
-{
-    return ble_gatt_chr_def {
-        .uuid = uuid,
-        .access_cb = callbackFunc,
-        .descriptors = descriptors,
-        .flags = flags,
-        .val_handle = handle,
-    };
-}
-
-
-[[Nodiscard]] ble_gatt_dsc_def make_descriptor(const ble_uuid_t* uuid, uint8_t attributeFlags, ble_gatt_access_fn callbackFunc) 
+[[Nodiscard]] const ble_gatt_dsc_def make_descriptor(const ble_uuid_t* pUuid, uint8_t attributeFlags, ble_gatt_access_fn callbackFunc) 
 {
     return ble_gatt_dsc_def {
-        .uuid = uuid,
+        .uuid = pUuid,
         .att_flags = attributeFlags,
         .access_cb = callbackFunc,
     };
 }
 
-[[Nodiscard]] ble_gatt_svc_def make_service(uint8_t serviceType, const ble_uuid_t* uuid, ble_gatt_chr_def* characteristics) 
+
+[[Nodiscard]] const ble_gatt_chr_def make_characteristic(const ble_uuid_t* pUuid, ble_gatt_access_fn callbackFunc,
+                                       ble_gatt_dsc_def* pDescriptors, uint8_t flags, uint16_t* pHandle) 
 {
-    return ble_gatt_svc_def {
-        .type = serviceType,
-        .uuid = uuid,
-        .characteristics = characteristics,
+    return ble_gatt_chr_def {
+        .uuid = pUuid,
+        .access_cb = callbackFunc,
+        .descriptors = pDescriptors,
+        .flags = flags,
+        .val_handle = pHandle,
     };
 }
 
 
-const ble_uuid128_t tmpServiceId =
-    BLE_UUID128_INIT(0x2d, 0x71, 0xa2, 0x59, 0xb4, 0x58, 0xc8, 0x12,
-                     0x99, 0x99, 0x43, 0x95, 0x12, 0x2f, 0x46, 0x59);                    
+[[Nodiscard]] const ble_gatt_svc_def make_service(uint8_t serviceType, const ble_uuid_t* pUuid, ble_gatt_chr_def* pCharacteristics) 
+{
+    return ble_gatt_svc_def {
+        .type = serviceType,
+        .uuid = pUuid,
+        .characteristics = pCharacteristics,
+    };
+}
+
+
+uint8_t tmpDescriptorValue; 
+const ble_uuid128_t tmpDescriptorId = BLE_UUID128_INIT(0x01, 0x01, 0x01, 0x01, 0x12, 0x12, 0x12, 0x12,
+                                                       0x23, 0x23, 0x23, 0x23, 0x34, 0x34, 0x34, 0x34);                 
 
 uint8_t tmpCharacteristicValue;
 uint16_t tmpCharacteristicValuehandle;
-const ble_uuid128_t tmpCharacteristicId =
-    BLE_UUID128_INIT(0x00, 0x00, 0x00, 0x00, 0x11, 0x11, 0x11, 0x11,
-                     0x22, 0x22, 0x22, 0x22, 0x33, 0x33, 0x33, 0x33);
+const ble_uuid128_t tmpCharacteristicId = BLE_UUID128_INIT(0x00, 0x00, 0x00, 0x00, 0x11, 0x11, 0x11, 0x11,
+                                                           0x22, 0x22, 0x22, 0x22, 0x33, 0x33, 0x33, 0x33);
 
+const ble_uuid128_t tmpServiceId = BLE_UUID128_INIT(0x2d, 0x71, 0xa2, 0x59, 0xb4, 0x58, 0xc8, 0x12,
+                                                    0x99, 0x99, 0x43, 0x95, 0x12, 0x2f, 0x46, 0x59);  
 
-uint8_t tmpDescriptorValue;
-const ble_uuid128_t tmpDescriptorId =
-    BLE_UUID128_INIT(0x01, 0x01, 0x01, 0x01, 0x12, 0x12, 0x12, 0x12,
-                     0x23, 0x23, 0x23, 0x23, 0x34, 0x34, 0x34, 0x34);
-
-
-int tmp_service_callback(uint16_t conn_handle, uint16_t attr_handle,
-                            struct ble_gatt_access_ctxt *ctxt, void *arg)
+int tmp_service_callback(uint16_t connectionHandle, uint16_t attributeHandle, // attributHandle means both characterisitics and descriptor, depending on who triggered the callback
+                            struct ble_gatt_access_ctxt* pContext, void* pArg)
 {
 
-    switch (ctxt->op) {
+    switch (pContext->op) {
     case BLE_GATT_ACCESS_OP_READ_CHR:
         break;
     case BLE_GATT_ACCESS_OP_WRITE_CHR:
@@ -73,7 +69,9 @@ int tmp_service_callback(uint16_t conn_handle, uint16_t attr_handle,
     default:
         break;
     }
-    assert(0);
+
+    // should never trigger
+    assert(0); // restarts the device
     return BLE_ATT_ERR_UNLIKELY;
 }
 
@@ -98,7 +96,7 @@ struct std::array<ble_gatt_chr_def, 2> characteristics_TEMPLATE =
 };
 
 
-struct std::array<ble_gatt_svc_def, 2> myService_TEMPLATE = 
+const struct std::array<ble_gatt_svc_def, 2> myService_TEMPLATE = 
 { 
     make_service(BLE_GATT_SVC_TYPE_PRIMARY, 
                     &tmpServiceId.u,
@@ -106,14 +104,9 @@ struct std::array<ble_gatt_svc_def, 2> myService_TEMPLATE =
     , 0
 }; 
 
-} // namespace
 
-
-CGattService::CGattService()
+void register_service(const struct ble_gatt_svc_def* pServices)
 {
-
-    ble_svc_gatt_init();
-
     int result = ble_gatts_count_cfg(myService_TEMPLATE.data());
     if (result != 0) {
         LOG_INFO("UNABLE TO COUNT GATT SVCS");
@@ -123,9 +116,20 @@ CGattService::CGattService()
     if (result != 0) {
         LOG_INFO("UNABLE TO ADD GATT SVCS");
     }
+}
+
+} // namespace
+
+
+CGattService::CGattService()
+{
+
+    ble_svc_gatt_init();
+
+    register_service(myService_TEMPLATE.data());
 
     /* Setting a value for the read-only descriptor */
-    tmpDescriptorValue = 0x99; // why are we setting it here?
+    tmpDescriptorValue = 0x99; // why are we setting it here? I cant see any difference when changing this value
 }
 
 } // application namespace 
