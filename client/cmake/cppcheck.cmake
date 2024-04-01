4af4b73abcded57f666223e5254f7094518d4c9d
christoffer larsson <christofferlarsson38@hotmail.com>
2024-04-01 15:56:41 +0200

    Moved icon id to its own header file so it can be included in cpp files as well as .rc file.
    Added helper script to check the source files to make sure no windows.h child headers are included.

--- client/cmake/cppcheck.cmake
+++ client/cmake/cppcheck.cmake
@@ -1,6 +1,15 @@
-add_custom_target(CPPCHECK
-    COMMAND /bin/bash cppcheck.sh
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    COMMENT "Running CppCheck.."
-    VERBATIM
-)
+if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+	add_custom_target(CPPCHECK
+		COMMAND cppcheck.sh
+		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+		COMMENT "Running CppCheck.."
+		VERBATIM
+	)
+else()
+	add_custom_target(CPPCHECK
+		COMMAND /bin/bash cppcheck.sh
+		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+		COMMENT "Running CppCheck.."
+		VERBATIM
+	)
+endif()
---
+++ client/src/resource.hpp
@@ -0,0 +1,6 @@
+//
+// Created by qwerty on 2024-04-01.
+//
+
+#pragma once
+#define IDI_APPICON 101
