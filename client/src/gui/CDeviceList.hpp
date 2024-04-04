ee1a5ee33d65d3a87ffce74177b5770d084a231c
christoffer larsson <christofferlarsson38@hotmail.com>
2024-04-04 23:13:53 +0200

    Added helper wrapper around raw pointer with implemented copy/move constructor/assignment.
    Added a basic device list gui that can start a scan.
    
    Added warning ignore to include of taskflow.hpp.
    
    Added function to CScanner that allows the retrieval of N devices. Added helper function to check state of CScanner.

---
+++ client/src/gui/CDeviceList.hpp
@@ -0,0 +1,38 @@
+//
+// Created by qwerty on 2024-04-03.
+//
+
+#pragma once
+#include "../../common/Pointer.hpp"
+#include "../bluetoothLE/Scanner.hpp"
+#include "../bluetoothLE/Device.hpp"
+#include "../common/CMutex.hpp"
+
+
+namespace gui
+{
+class CDeviceList
+{
+public:
+    explicit CDeviceList(ble::CScanner& scanner);
+    ~CDeviceList() = default;
+    CDeviceList(const CDeviceList& other);
+    CDeviceList(CDeviceList&& other) = default;
+    CDeviceList& operator=(const CDeviceList& other);
+    CDeviceList& operator=(CDeviceList&& other) = default;
+private:
+    void copy(const CDeviceList& other);
+public:
+    void push();
+private:
+    [[nodiscard]] auto time_limited_scan(std::chrono::seconds seconds);
+    void new_scan();
+    void device_list();
+private:
+    Pointer<ble::CScanner> m_pScanner = nullptr;
+    std::vector<ble::DeviceInfo> m_Devices;
+    std::unique_ptr<std::mutex> m_pMutex;
+private:
+    using mutex_t = std::remove_cvref_t<decltype(*m_pMutex)>;
+};
+}   // namespace gui
