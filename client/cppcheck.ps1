3aeb30abe7b1458309fbf9281c1ba2b8d441d097
christoffer larsson <christofferlarsson38@hotmail.com>
2024-04-14 00:39:29 +0200

    Uncommented build dependencies for Client (cppcheck and child header finder).
    Modified child header finder to only check direct child headers and not all the extra ones.
    
    Modified the cppcheck powershell script to now correctly recursively check all subdirectories.
    
    Removed cppcheck.sh since the client is only implemented for windows atm there is no reason to have a bash script.

--- client/cppcheck.ps1
+++ client/cppcheck.ps1
@@ -5,8 +5,9 @@ If (-Not (Test-Path -Path "build-cppcheck")) {
     New-Item -ItemType Directory -Path "build-cppcheck"
 }
 
-$command = "cppcheck --cppcheck-build-dir=build-cppcheck --error-exitcode=1 --enable=all --suppressions-list=.suppress.cppcheck --inline-suppr --std=c++23 src/*.cpp src/*.hpp"
-Invoke-Expression $command
+$files = Get-ChildItem -Path src, ../shared -Include *.cpp, *.hpp -Recurse | ForEach-Object { $_.FullName }
+cppcheck --cppcheck-build-dir=build-cppcheck --error-exitcode=1 --enable=all --suppressions-list=.suppress.cppcheck --inline-suppr --std=c++23 $files
+
 
 $result = $LASTEXITCODE
 
--- client/src/system/System.hpp
+++ client/src/system/System.hpp
@@ -5,6 +5,7 @@
 #pragma once
 #include "common/defines.hpp"
 // Taskflow must be included BEFORE windows.h
+// cppcheck-suppress unknownMacro
 IGNORE_WARNING_PUSH(4456)
 #include "taskflow/taskflow.hpp"
 IGNORE_WARNING_POP
